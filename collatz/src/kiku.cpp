d5a5e24be4c8fed38b3e075a86ca944e22bc6f33
mitsuboshi20190723 <53225096+mitsuboshi20190723@users.noreply.github.com>
2023-11-02 23:55:08 +0900

    Add files via upload

---
+++ collatz/src/kiku.cpp
@@ -0,0 +1,42 @@
+/*
+ * 2023.11.2
+ * kiku.cpp
+ * ver.0.1
+ * Kunihito Mitsuboshi
+ * license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+//#include <cstdio>
+//#include <memory>
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+namespace collatz
+{
+
+class KIKU : public rclcpp::Node
+{
+public:
+	__attribute__ ((visibility("default")))
+//	COMPOSITION_PUBLIC
+	explicit KIKU(const rclcpp::NodeOptions & opt) : Node("kiku", opt)
+	{
+		auto callback = [this](const std_msgs::msg::String::UniquePtr msg) -> void
+		{
+			RCLCPP_INFO(this->get_logger(), "%s", msg->data.c_str());
+		};
+
+		rclcpp::QoS qos(rclcpp::KeepLast(10));
+		sub_ = create_subscription<std_msgs::msg::String>("chatter", qos, callback);
+	}
+
+private:
+	rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
+};
+
+} /* namespace collatz */
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(collatz::KIKU)
